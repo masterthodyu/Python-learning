baad1e10165845407c3d942e53e1d2131b3effee
masterthodyu <masterthodyu@gmail.com>
2026-02-21 01:03:01 -0500

    Report Card lesson

---
+++ Certification_FreeCodeAcademy/Report_card.py
@@ -0,0 +1,12 @@
+name = 'Alice' #Variable
+print(name, type(name)) #Print output to terminal
+
+is_student = True # boolean
+print(is_student, type(is_student))
+
+age = 20 # integer
+print(age, type(age))
+
+score = 80.5 # float
+print(isinstance(score, float))
+
